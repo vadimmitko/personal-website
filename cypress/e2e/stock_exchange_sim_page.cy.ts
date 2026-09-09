1c5cacda5997b75c63361f6d71fe1d74e0aae876
Vadim Mitko <vadim@vadimmitko.com>
2026-09-09 16:47:22 +0100

    Removed redundant test

--- cypress/e2e/stock_exchange_sim_page.cy.ts
+++
@@ -1,7 +0,0 @@
-describe("Stock Exchange Simulator Page", () => {
-  it("Visits stock exchange simulator page", () => {
-    cy.visit("/");
-    cy.contains("Stock Exchange Simulator").click();
-    cy.url().should("include", "/stock-exchange-sim");
-  });
-});
